4aecce3fab9f61b908367454e95d79c776b2e0b4
SVGamage <119811762+SVGamage@users.noreply.github.com>
2025-01-19 15:22:21 +0530

    sidebar improvements

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Calendar,
+  GalleryVerticalEnd,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+} from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import SidebarUser from "./sidebar-user";
 
 // Menu items.
 const items = [
@@ -41,10 +51,30 @@ const items = [
     icon: Settings,
   },
 ];
+const user = {
+  name: "shadcn",
+  email: "m@example.com",
+  avatar: "/avatars/shadcn.jpg",
+};
+
+const header = {
+  name: "Virtual Careers Fair",
+  logo: GalleryVerticalEnd,
+};
 
 export function AppSidebar() {
   return (
     <Sidebar>
+      <SidebarHeader>
+        <div className="flex flex-row gap-2 w-full items-center justify-center p-1">
+          <div className="flex size-6 items-center justify-center rounded-sm border">
+            <header.logo className="size-4 shrink-0" />
+          </div>
+          <div className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-950 to-blue-600 dark:from-blue-600 dark:to-blue-200">
+            {header.name}
+          </div>
+        </div>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
@@ -64,6 +94,9 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarUser user={user} />
+      </SidebarFooter>
     </Sidebar>
   );
 }
