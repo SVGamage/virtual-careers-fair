3b61f8c6ae9a457d6829289ea1c27e311c7e4b27
SVGamage <119811762+SVGamage@users.noreply.github.com>
2025-05-02 02:41:14 +0530

    feat: implement authentication with better-auth and update UI components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="flex justify-end items-center p-4 gap-4 h-16"></header>
-        {children}
+        <main className="flex justify-center items-center min-h-screen bg-background text-foreground">
+          <header className="flex justify-end items-center p-4 gap-4 h-16"></header>
+          {children}
+        </main>
+
+        <Toaster />
       </body>
     </html>
   );
